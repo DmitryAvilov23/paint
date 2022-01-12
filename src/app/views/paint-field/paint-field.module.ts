2c714b01fa74d1a3cd2a702d3339e7743f743c0d
Dmitry Avilov <avilov.d@fmedia.tech>
2022-01-12 16:12:50 +0300

    feat: PaintFieldDirective: Created

--- src/app/views/paint-field/paint-field.module.ts
+++ src/app/views/paint-field/paint-field.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { PaintDirective } from './paint.directive';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PaintDirective
+  ],
   imports: [
     CommonModule
   ]
