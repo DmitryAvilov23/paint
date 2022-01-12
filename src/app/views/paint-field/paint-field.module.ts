7a5ad6cafe8f925ca469d1a04097070c29f1d1cc
Dmitry Avilov <avilov.d@fmedia.tech>
2022-01-12 19:21:23 +0300

    feat: ToolsPanel: Created

--- src/app/views/paint-field/paint-field.module.ts
+++ src/app/views/paint-field/paint-field.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PaintDirective } from './paint.directive';
+import { PaintFieldComponent } from './paint-field.component';
+import { ToolsPanelComponent } from './tools-panel/tools-panel.component';
 
 @NgModule({
   declarations: [
+    PaintFieldComponent,
+    ToolsPanelComponent,
     PaintDirective
   ],
   imports: [
