d08f9ea7f76c55340094c3e2a2f8affae605eb9e
Dmitry Avilov <avilov.d@fmedia.tech>
2022-01-12 14:19:35 +0300

    feat: PaintField: Created

---
+++ src/app/views/paint-field/paint-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paint-field',
+  templateUrl: './paint-field.component.html',
+  styleUrls: ['./paint-field.component.scss']
+})
+export class PaintFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
