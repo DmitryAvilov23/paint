e57c93897653bcd9f9922ecacf0b81e3f967dacd
Dmitry Avilov <avilov.d@fmedia.tech>
2022-01-13 16:36:19 +0300

    feat: RangeInputComponent: Created and completed

--- src/app/views/paint-field/tools-panel/range-input/range-input.component.ts
+++ src/app/views/paint-field/tools-panel/range-input/range-input.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PaintService } from '../../paint.service';
+
 @Component({
   selector: 'app-range-input',
   templateUrl: './range-input.component.html',
   styleUrls: ['./range-input.component.scss']
 })
 export class RangeInputComponent implements OnInit {
+  
+  public lineWidth: number = 1;
 
-  constructor() { }
+  constructor(private _paintService: PaintService) { }
 
   ngOnInit(): void {
   }
 
+  changeLineWidth() {
+    this._paintService.onLineWidthChange.emit(this.lineWidth);
+  }
 }
